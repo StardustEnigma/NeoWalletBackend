0d0a21bd3bb7b03d71cbc30234f912f59a9832b6
Atharva Mandle <atharvamandle19@gmail.com>
2026-01-11 21:27:06 +0530

    init

---
+++ src/main/java/com/neowallet/neowalletbackend/NeoWalletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neowallet.neowalletbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoWalletBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoWalletBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neowallet/neowalletbackend/NeoWalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neowallet.neowalletbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoWalletBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
